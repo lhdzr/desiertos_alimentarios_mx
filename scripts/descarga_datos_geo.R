273c5cccbf2219b959f91e074275f0a12f039b72
León Hernández Robles <leonhdzrobles@gmail.com>
2023-06-16 18:27:41 -0600

    Adecuación fórmulas, descarga de datos

--- scripts/descarga_datos_geo.R
+++ scripts/descarga_datos_geo.R
@@ -1,12 +1,33 @@
 source("tesina/funciones_descarga_inegi.R")
-source("tesina/codigos_municipales_nl.R")
 
-city_limits_directory <- "Datos/raw/limites_municipales/"
-ageb_output_dir <- "Datos/raw/agebs"
+# DESCARGA DE DATOS GEOGRAFICOS DE LOS AGEBS EN CADA ESTADO
+for (entidad in 1:32) {
+  descargaDatosAGEBUrb(entidad)
+}
 
-# Descarga de los archivos de límites municipales de municipios de Nuevo León
-downloadCityLimits(19,nl_codes,city_limits_directory)
-makeMunMap(19,nl_codes,city_limits_directory)
-# Descarga de los datos de los AGEB de los municipios de Nuevo León
-downloadAGEBData(19,nl_codes,ageb_output_dir)
+#DESCARGA DE DATOS ÍNDICE DE MARGINACIÓN URBANA
+IMU_2020 <- readxl::read_xls("datos/raw/imu_2020/IMU_2020/IMU_2020.xls", sheet = "IMU_2020") %>% 
+  filter(ENT==19) %>%
+  select(CVE_AGEB,IM_2020,GM_2020,IMN_2020)
+
+# INCORPORACIÓN DE TODOS LOS AGEB A UN MISMO DATAFRAME
+agebs_mx <- data.frame()
+for (entidad in 1:32) {
+  ent_str <- sprintf("%02d",entidad)
+  filepath <- paste0("datos/raw/agebs/ageb",ent_str,".geojson")
+  agebs_ent <- read_sf(filepath)
+  agebs_mx <- rbind(agebs_mx,agebs_ent)
+  print(paste("Entidad",ent_str,"agregada."))
+}
+
+# CRUZAR DATOS DE AGEBS CON DATOS DE INDICE DE MARGINACIÓN URBANA
+agebs_mx <- agebs_mx %>%
+  mutate(CVE_AGEB = paste0(cve_agee,cve_agem,cve_loc,cve_ageb)) %>%
+  left_join(IMU_2020,by="CVE_AGEB") %>% 
+  mutate(GM_2020 = factor(GM_2020, 
+                          levels = c("Muy bajo", "Bajo", "Medio", "Alto", "Muy alto"), 
+                          ordered = TRUE),
+         centroid = st_centroid(geometry),
+         pobtot = as.numeric(pobtot),
+         tvivhab = as.numeric(tvivhab))
 
--- scripts/funciones_descarga_inegi.R
+++ scripts/funciones_descarga_inegi.R
@@ -5,15 +5,17 @@ library(leaflet)
 library(tidyverse)
 
 
-catalogo_municipios <- read.csv("Datos/raw/catun_municipio/AGEEML_20233131250803.csv", encoding = "latin1")
+catalogo_municipios <- read.csv("datos/raw/catalogo_municipios/AGEEML_20236122149393.csv")
 
 
-getStateCodes <- function(nombre_ent) {
+# FUNCIÓN PARA OBTENER CÓDIGO DE ENTIDAD CON NOMBRE
+getCodigoEstado <- function(nombre_ent) {
   codes <- unique(catalogo_municipios$CVE_ENT[catalogo_municipios$NOM_ENT %in% nombre_ent])
   return(codes)
 }
-
-getMunCodes <- function(codigo_ent,nombres_mun=NULL){
+# FUNCIÓN PARA OBTENER CÓDIGO MUNICIPAL CON CÓDIGO DE ENTIDAD
+# Y NOMBRE DE MUNICIPIO
+getCodigoMunicipio <- function(codigo_ent,nombres_mun=NULL){
   if(is.null(nombres_mun)){
     codes <- unique(catalogo_municipios$CVE_MUN[catalogo_municipios$CVE_ENT == codigo_ent])
   }else{
@@ -22,20 +24,29 @@ getMunCodes <- function(codigo_ent,nombres_mun=NULL){
   return(codes)
 }
 
-downloadCityLimits <- function(entidad,codigos_mun,output_dir){
+
+# PROBABLEMENTE NO NECESARIAS. ÚTILES PARA HACER 
+# VISUALIZACIONES A NIVEL MUNICIPAL
+descargaLimitesMunicipales <- function(entidad){
+  # DIRECTORIO DE DESCARGA
+  lim_mun_path <- "datos/raw/limites_mun"
+  ifelse(!dir.exists(lim_mun_path),dir.create(lim_mun_path),print("Hi"))
+  lista_cve_mun <- unique(catalogo_municipios[catalogo_municipios$CVE_ENT==entidad,]$CVE_MUN)
+  
+  # LIGA DE DESCARGA
   base_url <- "https://gaia.inegi.org.mx/wscatgeo/geo/mgem/"
   ent_str <- sprintf("%02d", entidad)
   
-  for (code in codigos_mun) {
+  for (clave in lista_cve_mun) {
     # Formatear el código para que tenga tres dígitos
-    code_str <- sprintf("%03d", code)
+    code_str <- sprintf("%03d", clave)
     
     # Pegar el url y el código municipal
     url <- paste0(base_url,ent_str,code_str)
     
     # Definir el nombre del archivo
     filename <- paste0(ent_str, code_str, ".geojson")
-    filepath <- file.path(output_dir, filename)
+    filepath <- file.path(lim_mun_path, filename)
     
     # Hacer el GET del url
     response <- GET(url)
@@ -56,16 +67,15 @@ downloadCityLimits <- function(entidad,codigos_mun,output_dir){
     writeLines(geojson_string_trimmed, filepath)
   }
 }
-
-makeMunMap <- function(entidad,municipality_code_list,file_location) {
+makeMapaMunicipal <- function(entidad,lista_codigos_municipales,directorio) {
   ent_str <- sprintf("%02d", entidad)
   map <- leaflet() %>% addTiles()
   
-  for (code in municipality_code_list) {
+  for (code in lista_codigos_municipales) {
     code_str <- sprintf("%03d", code)
     # read the corresponding geojson file
-    file_name <- paste0(file_location,ent_str, code_str, ".geojson")
-    data <- read_sf(file_name)
+    filename <- paste0(directorio,"/",ent_str, code_str, ".geojson")
+    data <- read_sf(filename)
     
     # add the geojson layer to the map with the municipality name as the tooltip
     map <- map %>% addPolygons(data = data, fillColor = "blue",
@@ -76,9 +86,44 @@ makeMunMap <- function(entidad,municipality_code_list,file_location) {
   return(map)
 }
 
-downloadAGEBData <- function(entidad,codigos_mun,output_dir){
+
+# FUNCIÓN PARA DESCARGAR DATOS URBANOS LISTA
+descargaDatosAGEBUrb <- function(entidad){
+  # DIRECTORIO DE DESCARGA
+  data_ageb_path <- "datos/raw/agebs"
+  if (!dir.exists(data_ageb_path)) {
+    dir.create(data_ageb_path)
+  }
+  
   ent_str <- sprintf("%02d",entidad)
   agebs_base_url <- "https://gaia.inegi.org.mx/wscatgeo/geo/agebu/"
+  url <- paste0(agebs_base_url,ent_str)
+  response <- GET(url)
+  filename <- paste0("ageb",ent_str,".geojson")
+  filepath <- file.path(data_ageb_path, filename)
+  print(filepath)
+  if (status_code(response) == 200) {
+    # Guardar la respuesta al archivo con el nombre definido anteriormente
+    writeBin(rawToChar(content(response, as = "raw")), filepath)
+  } else {
+    cat(paste0("Error downloading data for state ", ent_str, ". Status code:", status_code(response_prueba), "\n"))
+  }
+  geojson_string <- readLines(filepath)
+  geojson_string_trimmed <- trimws(geojson_string, "right")
+  writeLines(geojson_string_trimmed, filepath)
+  cat(paste0("Data downloaded successfully for state ", ent_str, ".\n"))
+}
+# FUNCIÓN PARA DESCARGAR DATOS RURALES SÓLO NECESITA ADAPTACIONES
+descargaDatosAGEBRural <- function(entidad,codigos_mun,directorio){
+  ent_str <- sprintf("%02d",entidad)
+  agebs_base_url <- "https://gaia.inegi.org.mx/wscatgeo/geo/agebr/"
+  url <- paste0(agebs_base_url,ent_str,"/",code_str)
+  response <- GET(url)
+  filename <- paste0("ageb",ent_str,code_str,".geojson")
+  filepath <- file.path(lim_mun_path, filename)
+  geojson_string <- readLines(filepath)
+  geojson_string_trimmed <- trimws(geojson_string, "right")
+  writeLines(geojson_string_trimmed, filepath)
   
   for (code in codigos_mun) {
     code_str <- sprintf("%03d", code)
@@ -86,7 +131,7 @@ downloadAGEBData <- function(entidad,codigos_mun,output_dir){
     print(url)
     response <- GET(url)
     filename <- paste0("ageb",ent_str,code_str,".geojson")
-    filepath <- file.path(output_dir, filename)
+    filepath <- file.path(lim_mun_path, filename)
     print(filepath)
     
     if (status_code(response) == 200) {
@@ -102,17 +147,14 @@ downloadAGEBData <- function(entidad,codigos_mun,output_dir){
     writeLines(geojson_string_trimmed, filepath)
   }
 }
-
-makeAGEBMap <- function(entidad,mun_codes,fill_opacity){
+# FUNCIÓN PARA HACER MAPAS DE AGEBS A NIVEL ESTATAL LISTA
+makeAGEBMap <- function(entidad,fill_opacity){
   ent_str <- sprintf("%02d",entidad)
   data_completo <- data.frame()
+  filepath <- paste0('datos/raw/agebs/ageb',ent_str,'.geojson')
+  data <- read_sf(filepath)
+  data_completo <- rbind(data_completo,data)
   
-  for (code in mun_codes) {
-    mun_str <- sprintf("%03d",code)
-    filepath <- paste0('Datos/raw/agebs/ageb',ent_str,mun_str,'.geojson')
-    data <- read_sf(filepath)
-    data_completo <- rbind(data_completo,data)
-  }
   #%>% addTiles()
   map <- leaflet() %>%  addProviderTiles("CartoDB.Positron") %>% addPolygons(data = data_completo, fillColor = "blue",
                                                                              stroke = TRUE, weight = .2, opacity = 1,
