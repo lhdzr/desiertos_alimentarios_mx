fbaa88ecb1931872e367fd17f8aefd42d88e6c94
León Hernández Robles <leonhdzrobles@gmail.com>
2023-06-17 17:58:37 -0600

    Datos DENUE

--- scripts/descarga_datos_geo.R
+++ scripts/descarga_datos_geo.R
@@ -1,4 +1,5 @@
-source("tesina/funciones_descarga_inegi.R")
+library(tidyverse)
+source("scripts/funciones_descarga_inegi.R")
 
 # DESCARGA DE DATOS GEOGRAFICOS DE LOS AGEBS EN CADA ESTADO
 for (entidad in 1:32) {
@@ -15,8 +16,7 @@ agebs_mx <- data.frame()
 for (entidad in 1:32) {
   ent_str <- sprintf("%02d",entidad)
   filepath <- paste0("datos/raw/agebs/ageb",ent_str,".geojson")
-  agebs_ent <- read_sf(filepath)
-  agebs_mx <- rbind(agebs_mx,agebs_ent)
+  agebs_mx <- rbind(agebs_mx,read_sf(filepath))
   print(paste("Entidad",ent_str,"agregada."))
 }
 
@@ -31,3 +31,26 @@ agebs_mx <- agebs_mx %>%
          pobtot = as.numeric(pobtot),
          tvivhab = as.numeric(tvivhab))
 
+# LEER DATOS DENUE
+paths <-c("datos/raw/denue/denue_00_46111_shp/conjunto_de_datos/denue_inegi_46111_.shp",
+          "datos/raw/denue/denue_00_46112-46311_shp/conjunto_de_datos/denue_inegi_46112-46311_.shp",
+          "datos/raw/denue/denue_00_72_2_0522_shp/conjunto_de_datos/denue_inegi_72_2.shp")
+denue <- data.frame()
+
+for (path in paths) {
+  units <- read_sf(path) %>%
+    select(id,clee,nom_estab,raz_social,
+           codigo_act,nombre_act,per_ocu,
+           cve_ent,cve_mun,ageb,manzana,
+           latitud,longitud,geometry)
+  denue <- rbind(denue,units)
+  print(paste("Path",path,"added."))
+}
+
+denue <- denue %>%
+  filter(grepl("^(7225|4611)", codigo_act))
+
+Encoding(denue$nom_estab) <- "latin1"
+Encoding(denue$nombre_act) <- "latin1"
+
+denue$alimento_convencional <- 1
